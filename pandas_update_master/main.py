69d7791bbad667d98bd7259fbd843d3c55150645
lucaandradet <64807231+lucaandradet@users.noreply.github.com>
2021-03-26 21:37:47 -0300

    Correção valor da linha

--- pandas_update_master/main.py
+++ pandas_update_master/main.py
@@ -9,7 +9,7 @@ now = datetime.now() #Pegando a data e hora de agora
 data_hora = now.strftime("%d/%m/%Y %H:%M:%S") #Definindo formato DD/MM/YYYY HH:MM:SS para a hora pega
 
 #Começando a contar as linhas do csv depois da primeira linha do cabeçalho. Ex. Cabeçalho,0,1,2,3,4,...
-df.loc[1, 'UltimaHora'] = data_hora #Atualizando o valor da linha [0], coluna = [UltimaHora] = Data e hora no novo formato
+df.loc[0, 'UltimaHora'] = data_hora #Atualizando o valor da linha [0], coluna = [UltimaHora] = Data e hora no novo formato
 
 df.to_csv("toque.csv", index=False, sep=';', encoding='utf8') #Escrevendo novo valor no arquivo CSV com delimitador (;)
 
